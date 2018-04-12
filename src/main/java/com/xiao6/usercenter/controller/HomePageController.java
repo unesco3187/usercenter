309af280ed40843a32c9467990530236bd37a911
unesco3187 <309398671@qq.com>
2018-04-12 12:07:15 +0800

    index

---
+++ src/main/java/com/xiao6/usercenter/controller/HomePageController.java
@@ -0,0 +1,17 @@
+package com.xiao6.usercenter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author meiwenjun
+ * @Description
+ * @create 2018-04-12 12:05
+ **/
+@Controller
+public class HomePageController {
+    @RequestMapping("/")
+    public String index() {
+        return "forward:/index.html";
+    }
+}
